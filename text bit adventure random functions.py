dd4e2fd3594ab37f5303de00e64a7f7f5f59c47b
Nobody Fear <nobodyf34r@gmail.com>
2022-06-14 16:45:51 +0800

    42720

--- text bit adventure random functions.py
+++ text bit adventure random functions.py
@@ -6,7 +6,7 @@ def e():
  a(c(4),3);b(57);b(59);b();print(f"\nEnemy health is {c(1)}.\n")
  while c(1)>0:
   a(ord((input("Do you "+(""if A[60]+A[59]==2 else"attack, ")+"potion, defend or look at stats?\n: ")+" ").lower()[0]),6)
-  while c(6)==112 and c(5)==0 or c(6)!=97 and c(6)!=112 and c(6)!=100 or c(6)==97 and A[60]+A[59]==2:print("\x1B[2JNo more potions!\n")if c(6)==112 else print("\x1B[2JToo exausted.\n")if c(6)==97 and A[60]+A[59]==2 else print(f"\x1B[2JPlayer stats:\npotion amount: {c(5)}\ncurrent health: {c(3)}\nmax health: {c(4)}\nstrength: {c(2)}\n\nenemy stats:\nhealth: {c(1)}\nstrength: {c()}\n"+("\nNice.\n"if c(5)==69 or c(3)==69 or c(5)==169 or c(3)==169 else""))if c(6)==115 or c(6)==108 else print("\x1B[2JInvalid action.\n");a(ord((input("Do you "+(""if A[60]+A[59]==2 else"attack, ")+"potion, defend or look at stats?\n: ")+" ").lower()[0]),6)
+  while(c(6),c(5))==(112,0)or not c(6)in[97,112,100]or(c(6),A[60]+A[59])==(97,2):print("\x1B[2JNo more potions!\n")if c(6)==112 else print("\x1B[2JToo exausted.\n")if(c(6),A[60]+A[59])==(97,2)else print(f"\x1B[2JPlayer stats:\npotion amount: {c(5)}\ncurrent health: {c(3)}\nmax health: {c(4)}\nstrength: {c(2)}\n\nenemy stats:\nhealth: {c(1)}\nstrength: {c()}\n"+("\nNice.\n"if c(5)==69 or c(3)==69 or c(5)==169 or c(3)==169 else""))if c(6)==115 or c(6)==108 else print("\x1B[2JInvalid action.\n");a(ord((input("Do you "+(""if A[60]+A[59]==2 else"attack, ")+"potion, defend or look at stats?\n: ")+" ").lower()[0]),6)
   if c(6)==97:print("\x1B[2JYou attack!\nYou do",c(2),"damage!");a(0 if c(1)-c(2)<1 else c(1)-c(2),1);print(f"Enemy health is now {c(1)}.");b(63);b(59 if A[60]==1 else 60,1)
   elif c(6)==112:
    if 5+c(3)>c(4):print("\x1B[2JYou restore",c(4)-c(3),"health!");a(c(4),3)
@@ -14,13 +14,13 @@ def e():
    a(c(5)-1,5);print("You have 1 potion left!"if c(5)==1 else f"You have {c(5)} potions left!"+("\n\nNice."if c(5)==69 or c(5)==169 else""));b(63);b();b(59)
   else:print("\x1B[2JYou defend!");b(63,1);b();b(59)
   if c(1)>0:
-   if hash(str(A))%3==0 and A[57]==0 and A[60]==1:b(57,1);a(255 if c(1)+2>255 else c(1)+2,1);print("\nEnemy heals!\nEnemy gains 2 health.\nEnemy has",c(1),"health remaining!\n")
+   if(hash(str(A))%3,A[57],A[60])==(0,0,1):b(57,1);a(255 if c(1)+2>255 else c(1)+2,1);print("\nEnemy heals!\nEnemy gains 2 health.\nEnemy has",c(1),"health remaining!\n")
    else:
     b(57);print("\nEnemy attacks!")
     if A[63]==1:print("Enemy does",int(c()/2),"damage!");a(0 if c(3)-int(c()/2)<1 else c(3)-int(c()/2),3)
     else:print("Enemy does",c(),"damage!");a(0 if c(3)-c()<1 else c(3)-c(),3)
     print("You have",c(3),"health remaining!\n"+("\nNice.\n"if c(3)==69 or c(3)==169 else""))
-   if c(3)<1:print("SECRET ENDING\n\nOh! why hello there!\nIt's me,\nthe creator of this game!\nI just wanted to personally congratulate you on getting this ending.\nEven after getting god-like powers, you still laid down your sword and let the world return to its balance.\nYou are amazing and have shown so much dedication and patience to this game and i commend you for that!\nNo matter what they are, you can achieve your dreams!\nDon't let anything stop you!\nAnyways it's my time to go now.\nWith all that said,\ngoodbye player!"if c(2)==255 and c(4)==255 and A[61]==1 else"You die!");b(62,1);break
+   if c(3)<1:print("SECRET ENDING\n\nOh! why hello there!\nIt's me,\nthe creator of this game!\nI just wanted to personally congratulate you on getting this ending.\nEven after getting god-like powers, you still laid down your sword and let the world return to its balance.\nYou are amazing and have shown so much dedication and patience to this game and i commend you for that!\nNo matter what they are, you can achieve your dreams!\nDon't let anything stop you!\nAnyways it's my time to go now.\nWith all that said,\ngoodbye player!"if(c(2),c(4),A[61])==(255,255,1)else"You die!");b(62,1);break
 def f():
  a(ord((input("Would you like to increase your max health or strength?\n: ")+" ").lower()[0]),1)
  while c(1)!=104 and c(1)!=109 and c(1)!=115:print("\x1B[2JInvalid option.\n");a(ord((input("Would you like to increase your max health or strength?\n: ")+" ").lower()[0]),1)
@@ -38,12 +38,12 @@ if input("\x1B[2JHint, if you want to beat the game, upgrade your health by at l
 else:input('\x1B[2JPsst, one code is "poti"! (enter to continue)');print("\x1B[2J")
 a(5,4);a(2,2);a(2,5)
 if c()==9:a();a(20,4);a(20,2);a(20,5)
-if c()==8:a(5);(20,2)
+if c()==8:a(5);a(20,2)
 if c()==7:a();a(100,5)
 if c()==6:b(56);a(5);a(255,4);a(255,2);a(255,5);b(61,1)
 if c()==1:a(5,1);d();a(2);h();print(" small rodent!");e()
-if c()==2 and A[62]==0:j();print("rodent was defeated!\n");f();g();d();a(3);a(4,1);h();print(" evil ghoul!");e()
-if c()==3 and A[62]==0:j();print("ghoul was defeated!\n");f();g();d();a(2);a(10,1);h();print("n evil lake monster!");e();a(4)
-if c()==4 and A[62]==0:j();print("lake monster was defeated!\n");f();g();d();print("\x1B[2JYou find an XP orb!\n");a(5);f()
+if(c(),A[62])==(2,0):j();print("rodent was defeated!\n");f();g();d();a(3);a(4,1);h();print(" evil ghoul!");e()
+if(c(),A[62])==(3,0):j();print("ghoul was defeated!\n");f();g();d();a(2);a(10,1);h();print("n evil lake monster!");e();a(4)
+if(c(),A[62])==(4,0):j();print("lake monster was defeated!\n");f();g();d();print("\x1B[2JYou find an XP orb!\n");a(5);f()
 if c()==5:input("\nBOSS (enter to continue)");h();print(" massive evil duck!");a(3);a(20,1);e()
-print("\x1B[2JGreat, now try beat it without cheats!\n"if A[56]==1 and A[62]==0 else"\x1B[2JThe huge evil duck was defeated and peace was restored to the land!\n"if A[62]==0 else"",end="");print("\nhttps://bit.ly/382Jahf"if c(2)==255 and c(4)==255 and c(5)==0 else"\nhttps://bit.ly/3wcgAlt"if A[61]==1 and(c(5)==169 or c(5)==69)and(c(3)==169 or c(3)==69)else"",end="");print("\nEND.\n")
+print("\x1B[2JGreat, now try beat it without cheats!\n"if(A[56],A[62])==(1,0)else"\x1B[2JThe huge evil duck was defeated and peace was restored to the land!\n"if A[62]==0 else"",end="");print("\nhttps://bit.ly/382Jahf"if(c(2),c(4),c(5))==(255,255,0)else"\nhttps://bit.ly/3wcgAlt"if (A[61],c(5)in[169,69],c(3)in[169,69])==(1,True,True)else"",end="");print("\nEND.\n")
