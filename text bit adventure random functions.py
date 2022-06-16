34990908cdaeb018da5e37d07d4a66205a8e4176
Nobody Fear <nobodyf34r@gmail.com>
2022-06-16 14:19:24 +0800

    looks bigger but actually fixes a gamebreaking bug

--- text bit adventure random functions.py
+++ text bit adventure random functions.py
@@ -1,4 +1,4 @@
-def a(B=1,C=0):global A;A=[int(X)for Y in[list(7*"0"+bin(0 if B<0 else B)[2:])[-8:]if i==C else A[i*8:i*8+8]for i in range(8)]for X in Y]
+def a(B=1,C=0):global A;A=[int(X)for Y in[list(7*"0"+bin(0 if B<0 else B)[2:])[-8:]if i==C else A[i*8:i*8+8]for i in range(8)]for X in Y];return B
 def b(C=60,D=0):global A;A=[(0 if D!=1 else 1)if i==C else A[i]for i in range(64)]
 def c(C=0):return int(''.join([str(A[i+8*C])for i in range(8)]),2)
 def d():input(f"Level {c()}! (enter to continue)")
@@ -7,14 +7,14 @@ def e():
  while c(1)>0:
   a(0,6)
   while(c(6),c(5))==(112,0)or not c(6)in[97,112,100]or(c(6),A[60]+A[59])==(97,2):print("\x1B[2JNo more potions!\n"if c(6)==112 else"\x1B[2JToo exausted.\n"if(c(6),A[60]+A[59])==(97,2)else str(print("\x1B[2JPlayer stats:\npotion amount:",c(5),"\ncurrent health:",c(3),"\nmax health:",c(4),"\nstrength:",c(2),"\n\nenemy stats:\nhealth:",c(1),"\nstrength:",c(),("\n\nNice."if c(5)in[69,169]or c(3)in[169,69]else"")))[:-4]if c(6)in[115,108]else"\x1B[2JInvalid action.\n"if c(6)!=0 else"");a(ord((input("Do you "+(""if A[60]+A[59]==2 else"attack, ")+"potion, defend or look at stats?\n: ")+" ").lower()[0]),6)
-  if c(6)==97:print("\x1B[2JYou attack!\nYou do",c(2),"damage!");a(c(1)-c(2),1);print(f"Enemy health is now {c(1)}.");b(63);b(59 if A[60]==1 else 60,1)
+  if c(6)==97:print("\x1B[2JYou attack!\nYou do",c(2),"damage!");print(f"Enemy health is now {a(c(1)-c(2),1)}.");b(63);b(59 if A[60]==1 else 60,1)
   elif c(6)==112:
    if 5+c(3)>c(4):print("\x1B[2JYou restore",c(4)-c(3),"health!");a(c(4),3)
    else:print("\x1B[2JYou restore 5 health!");a(c(3)+5,3)
-   a(c(5)-1,5);print("You have",str(c(5)),"potion"+("s"if c(5)!=1 else""),"left!"+("\n\nNice."if c(5)in[69,169]else""));b(63);b();b(59)
+   print("You have",str(a(c(5)-1,5)),"potion"+("s"if c(5)!=1 else""),"left!"+("\n\nNice."if c(5)in[69,169]else""));b(63);b();b(59)
   else:print("\x1B[2JYou defend!");b(63,1);b();b(59)
   if c(1)>0:
-   if(hash(str(A))%3,A[57],A[60])==(0,0,1):b(57,1);a(255 if c(1)+2>255 else c(1)+2,1);print("\nEnemy heals!\nEnemy gains 2 health.\nEnemy has",c(1),"health remaining!")
+   if(hash(str(A))%3,A[57],A[60])==(0,0,1):b(57,1);print("\nEnemy heals!\nEnemy gains 2 health.\nEnemy has",a(255 if c(1)+2>255 else c(1)+2,1),"health remaining!")
    else:
     b(57);print("\nEnemy attacks!")
     if A[63]==1:print("Enemy does",int(c()/2),"damage!");a(c(3)-int(c()/2),3)
@@ -43,6 +43,6 @@ if c()==6:b(56);a(5);a(255,4);a(255,2);a(255,5);b(61,1)
 if c()==1:a(5,1);d();a(2);h();print(" small rodent!");e()
 if(c(),A[62])==(2,0):j();print("rodent was defeated!\n");f();g();d();a(3);a(4,1);h();print(" evil ghoul!");e()
 if(c(),A[62])==(3,0):j();print("ghoul was defeated!\n");f();g();d();a(2);a(10,1);h();print("n evil lake monster!");e();a(4)
-if(c(),A[62])==(4,0):j();print("lake monster was defeated!\n");f();g();d();print("\x1B[2JYou find an XP orb!\n");a(5);f()
+if(c(),A[62])==(4,0):j();print("lake monster was defeated!\n");f();g();d();print("\x1B[2JYou find an XP orb!\n");a(5);a(0,1);f()
 if c()==5:input("\nBOSS (enter to continue)");h();print(" massive evil duck!");a(3);a(20,1);e()
 print("\x1B[2JGreat, now try beat it without cheats!\n"if(A[56],A[62])==(1,0)else"\x1B[2JThe huge evil duck was defeated and peace was restored to the land!\n"if A[62]==0 else"",end="");print("\nhttps://bit.ly/382Jahf\n"if(c(2),c(4),c(5))==(255,255,0)else"\nhttps://bit.ly/3wcgAlt\n"if(A[61],c(5)in[169,69],c(3)in[169,69])==(1,True,True)else"",end="");print("\nEND.\n")
