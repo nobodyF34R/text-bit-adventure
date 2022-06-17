71c50565bb705141df6a0fecb8edd870409ffc57
Nobody Fear <nobodyf34r@gmail.com>
2022-06-17 15:17:47 +0800

    39936 + new notext rules

--- notext.py
+++ notext.py
@@ -1,36 +1,48 @@
-def a(B=1,C=0):global A;A=[int(X)for Y in[list(7*"0"+bin(B)[2:])[-8:]if i==C else A[i*8:i*8+8]for i in range(8)]for X in Y]
+def a(B=1,C=0):global A;A=[int(X)for Y in[list(7*"0"+bin(0 if B<0 else B)[2:])[-8:]if i==C else A[i*8:i*8+8]for i in range(8)]for X in Y];return B
 def b(C=60,D=0):global A;A=[(0 if D!=1 else 1)if i==C else A[i]for i in range(64)]
 def c(C=0):return int(''.join([str(A[i+8*C])for i in range(8)]),2)
-def d():input()
+def d():input(f"")
 def e():
- a(c(4),3);b(57);b(59);b()
+ a(c(4),3);b(57);b(59);b();print(f"")
  while c(1)>0:
   a(0,6)
-  while(c(6),c(5))==(112,0)or not c(6)in[97,112,100]or(c(6),A[60]+A[59])==(97,2):a(ord((input()+" ").lower()[0]),6)
-  if c(6)==97:a(0 if c(1)-c(2)<1 else c(1)-c(2),1);b(63);b(59 if A[60]==1 else 60,1)
+  while(c(6),c(5))==(112,0)or not c(6)in[97,112,100]or(c(6),A[60]+A[59])==(97,2):print(""if c(6)==112 else""if(c(6),A[60]+A[59])==(97,2)else str(print("",c(5),"",c(3),"",c(4),"",c(2),"",c(1),"",c(),(""if c(5)in[69,169]or c(3)in[169,69]else"")))[:-4]if c(6)in[115,108]else""if c(6)!=0 else"");a(ord((input(""+(""if A[60]+A[59]==2 else"")+"")+" ").lower()[0]),6)
+  if c(6)==97:print("",c(2),f"");b(63);b(59 if A[60]==1 else 60,1)
   elif c(6)==112:
-   if 5+c(3)>c(4):a(c(4),3)
-   else:a(c(3)+5,3)
-   a(c(5)-1,5);b(63);b();b(59)
-  else:b(63,1);b();b(59)
+   if 5+c(3)>c(4):print("");a(c(4),3)
+   else:print("");a(c(3)+5,3)
+   print("",str(a(c(5)-1,5)),""+(""if c(5)!=1 else""),""+(""if c(5)in[69,169]else""));b(63);b();b(59)
+  else:print("");b(63,1);b();b(59)
   if c(1)>0:
-   if(hash(str(A))%3,A[57],A[60])==(0,0,1):b(57,1);a(255 if c(1)+2>255 else c(1)+2,1)
+   if(hash(str(A))%3,A[57],A[60])==(0,0,1):b(57,1);print("",a(255 if c(1)+2>255 else c(1)+2,1),"")
    else:
-    b(57)
-    if A[63]==1:a(0 if c(3)-int(c()/2)<1 else c(3)-int(c()/2),3)
-    else:a(0 if c(3)-c()<1 else c(3)-c(),3)
-   if c(3)<1:b(62,1);break
+    b(57);print("")
+    if A[63]==1:print("",int(c()/2),"");a(c(3)-int(c()/2),3)
+    else:print("",c(),"");a(c(3)-c(),3)
+    print("",c(3),""+(""if c(3)in[69,169]else""))
+   if c(3)<1:print(""if(c(4),A[61])==(255,1)else"");b(62,1);break
 def f():
- while c(1)!=104 and c(1)!=109 and c(1)!=115:a(ord((input()+" ").lower()[0]),1)
- if c(1)in[104,109]:a(c(4)+1,4)
- else:a(c(2)+1,2)
+ while c(1)!=104 and c(1)!=109 and c(1)!=115:a(ord((input((""if c(1)!=0 else"")+"")+" ").lower()[0]),1)
+ if c(1)in[104,109]:a(c(4)+1,4);print("")
+ else:a(c(2)+1,2);print("")
+def g():print("",c(),"");a(c(5)+c(),5)
+def h():print("",end="")
+def j():print("",end="")
 A=[0]*64;a()
-if input().lower()[:1]=="y":
- input();input()
-else:input()
+if input("").lower()[:1]=="y":
+ for i in input("")[:4].lower():a(ord(i),c(6));a(c(6)+1,6)
+ a(1)
+ if c()==1:input("");print("\x1B[2J")
+ else:b(56,1);print("")
+else:input('');print("\x1B[2J")
 a(5,4);a(2,2);a(2,5)
-if c()==1:a(5,1);d();a(2);e()
-if(c(),A[62])==(2,0):f();d();a(3);a(4,1);e()
-if(c(),A[62])==(3,0):f();d();a(2);a(10,1);e();a(4)
-if(c(),A[62])==(4,0):f();d();a(5);f()
-if c()==5:input();a(3);a(20,1);e()
+if c()==9:a();a(20,4);a(20,2);a(20,5)
+if c()==8:a(5);a(20,2)
+if c()==7:a();a(100,5)
+if c()==6:b(56);a(5);a(255,4);a(255,2);a(255,5);b(61,1)
+if c()==1:a(5,1);d();a(2);h();print("");e()
+if(c(),A[62])==(2,0):j();print("");f();g();d();a(3);a(4,1);h();print("");e()
+if(c(),A[62])==(3,0):j();print("");f();g();d();a(2);a(10,1);h();print("");e();a(4)
+if(c(),A[62])==(4,0):j();print("");f();g();d();print("");a(5);a(0,1);f()
+if c()==5:input("");h();print("");a(3);a(20,1);e()
+print(""if(A[56],A[62])==(1,0)else""if A[62]==0 else"",""if(c(2),c(4),c(5))==(255,255,0)else""if(A[61],c(5)in[169,69],c(3)in[169,69])==(1,True,True)else"","")
